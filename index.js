93bfcf5dab8d38c309d9aeb2b6e1af664a5ab36c
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-07-27 14:10:42 +0900

    Add: 자바스크립트로 데이터 적용

--- index.js
+++ index.js
@@ -11,23 +11,50 @@ queryParams += `&${encodeURIComponent("resultType")}=${encodeURIComponent(
 )}`;
 
 // 책 앞에 Fetch 예제를 이용하여 데이터 가져오기
+let foodList = [];
 async function getData() {
   try {
     const foods = await fetch(
       `http://apis.data.go.kr/6260000/FoodService/getFoodKr${queryParams}`
     ).then((res) => res.json());
-    const foodList = foods.getFoodKr.item;
-    foodList.map((item, index) => {
-      console.log(
-        `${index + 1}번째 맛집은 ${item.MAIN_TITLE}입니다.
-      주소: ${item.ADDR1}
-      전화번호: ${item.CNTCT_TEL}
-      설명: ${item.ITEMCNTNTS}
-      대표 이미지: ${item.MAIN_IMG_NORMAL}`
-      );
-    });
+    foodList = foods.getFoodKr.item;
+    setFoodCard();
   } catch (err) {
     console.error(err);
   }
 }
 getData();
+const indexNum = +location.pathname.slice(6, 7);
+
+function setFoodCard() {
+  for (let i = 9 * (indexNum - 1); i < 9 * indexNum; i++) {
+    let item = foodList[i];
+    document.querySelector(".card-wrap").insertAdjacentHTML(
+      "beforeend",
+      `
+    <div class="card">
+            <span class="gu-name" id="gugun_name">${item.GUGUN_NM}</span>
+            <img src=${item.MAIN_IMG_NORMAL} alt="맛집 사진" class="main-img" id="main-img-thumb" >
+            <div class="main-info">
+            <strong class="main-title" id="main-title">${item.MAIN_TITLE}</strong>
+            <strong class="main-tel" id="cntct-tel">${item.CNTCT_TEL}</strong>
+            </div>
+            <p class="content" id="itemcntnts">${item.ITEMCNTNTS}</p>
+            <dl class="info-list">
+              <dt class="info-addr">주 소</dt>
+              <dd id="addr1">${item.ADDR1}</dd>
+            </dl>
+            <dl class="info-list">
+              <dt>영업시간</dt>
+              <dd id="usage-day-week-and-time">${item.USAGE_DAY_WEEK_AND_TIME}</dd>
+            </dl>
+            <dl class="info-list">
+              <dt>대표메뉴</dt>
+              <dd id="rpesntv-menu">${item.RPRSNTV_MENU}</dd>
+            </dl>
+            <img class="like-wrap" src="./src/images/icon/heart-full.svg" alt="좋아요">
+          </div>
+    `
+    );
+  }
+}
