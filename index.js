012412cc8d09c1116f171cd4af46df23d14e5643
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-07-27 15:01:11 +0900

    Update: 페이지네이션

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const pagination = document.querySelector(".pagenation-wrap");
+const output = document.querySelector(".card-wrap");
 // 공공 데이터 가져오기
 let queryParams = `?${encodeURIComponent(
   "serviceKey"
@@ -10,53 +12,95 @@ queryParams += `&${encodeURIComponent("resultType")}=${encodeURIComponent(
   "json"
 )}`;
 
+const dataObj = {
+  datasPerPage: 9,
+  currentPage: 1,
+  datas: null,
+};
+
+window.addEventListener("load", () => {
+  init();
+});
+
 // 책 앞에 Fetch 예제를 이용하여 데이터 가져오기
-let foodList = [];
-async function getData() {
+async function init() {
   try {
     const foods = await fetch(
       `http://apis.data.go.kr/6260000/FoodService/getFoodKr${queryParams}`
     ).then((res) => res.json());
-    foodList = foods.getFoodKr.item;
-    setFoodCard();
+    dataObj.datas = foods.getFoodKr.item;
+    loadPage(9);
   } catch (err) {
     console.error(err);
   }
 }
-getData();
-const indexNum = +location.pathname.slice(6, 7);
 
-function setFoodCard() {
-  for (let i = 9 * (indexNum - 1); i < 9 * indexNum; i++) {
-    let item = foodList[i];
-    document.querySelector(".card-wrap").insertAdjacentHTML(
-      "beforeend",
-      `
-    <div class="card">
-            <span class="gu-name" id="gugun_name">${item.GUGUN_NM}</span>
-            <img src=${item.MAIN_IMG_NORMAL} alt="맛집 사진" class="main-img" id="main-img-thumb" >
-            <div class="main-info">
-            <strong class="main-title" id="main-title">${item.MAIN_TITLE}</strong>
-            <strong class="main-tel" id="cntct-tel">${item.CNTCT_TEL}</strong>
-            </div>
-            <p class="content" id="itemcntnts">${item.ITEMCNTNTS}</p>
-            <dl class="info-list">
-              <div class="info-wrap">
-                <dt class="info-addr">주 소</dt>
-                <dd id="addr1">${item.ADDR1}</dd>
-              </div>
-              <div class="info-wrap">
-                <dt>영업시간</dt>
-                <dd id="usage-day-week-and-time">${item.USAGE_DAY_WEEK_AND_TIME}</dd>
-              </div>
-              <div class="info-wrap">
-                <dt>대표메뉴</dt>
-                <dd id="rpesntv-menu">${item.RPRSNTV_MENU}</dd>
-              </div>
-            </dl>
-            <img class="like-wrap" src="./src/images/icon/heart-full.svg" alt="좋아요">
-          </div>
-    `
-    );
+// 페이지를 로딩하는 함수입니다.
+function loadPage(page) {
+  dataObj.currentPage = page;
+
+  let startPost = (dataObj.currentPage - 1) * dataObj.datasPerPage;
+
+  let totalPages = Math.ceil(dataObj.datas.length / dataObj.datasPerPage);
+
+  let endPost =
+    startPost + dataObj.datasPerPage > dataObj.datas.length
+      ? dataObj.datas.length
+      : startPost + dataObj.datasPerPage;
+
+  output.innerHTML = "";
+
+  // 받아온 데이터를 순환합니다.
+  for (let i = startPost; i < endPost; i++) {
+    console.log(dataObj.datas[i]);
+    let item = dataObj.datas[i];
+
+    let card = document.createElement("article");
+    card.innerHTML = `
+      <div class="card">
+      <span class="gu-name" id="gugun_name">${item.GUGUN_NM}</span>
+      <img src=${item.MAIN_IMG_NORMAL} alt="맛집 사진" class="main-img" id="main-img-thumb" >
+      <div class="main-info">
+      <strong class="main-title" id="main-title">${item.MAIN_TITLE}</strong>
+      <strong class="main-tel" id="cntct-tel">${item.CNTCT_TEL}</strong>
+      </div>
+      <p class="content" id="itemcntnts">${item.ITEMCNTNTS}</p>
+      <dl class="info-list">
+        <dt class="info-addr">주 소</dt>
+        <dd id="addr1">${item.ADDR1}</dd>
+      </dl>
+      <dl class="info-list">
+        <dt>영업시간</dt>
+        <dd id="usage-day-week-and-time">${item.USAGE_DAY_WEEK_AND_TIME}</dd>
+      </dl>
+      <dl class="info-list">
+        <dt>대표메뉴</dt>
+        <dd id="rpesntv-menu">${item.RPRSNTV_MENU}</dd>
+      </dl>
+      <img class="like-wrap" src="./src/images/icon/heart-full.svg" alt="좋아요">
+    </div>
+      `;
+    output.append(card);
   }
+
+  paginationMaker(totalPages);
+}
+
+function paginationMaker(totalPages) {
+  pagination.innerHTML = "";
+  const container = document.createDocumentFragment();
+
+  for (let i = 0; i < totalPages; i++) {
+    const item = document.createElement("li");
+    item.textContent = i + 1;
+    item.addEventListener("click", () => {
+      loadPage(i + 1);
+    });
+
+    if (i + 1 === dataObj.currentPage) {
+      item.classList.add("active");
+    }
+    container.append(item);
+  }
+  pagination.append(container);
 }
